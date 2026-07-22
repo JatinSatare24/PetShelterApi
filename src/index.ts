32724c23623aac14d9a3202c486479a6c019b2ac
Jatin Satare <jatinsatare@gmail.com>
2026-07-22 17:41:54 +0530

    listen on express app & add 404 response logic

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,10 @@ import {petRouter} from './routes/pets.routes'
 const PORT = 8000
 const app:Express = express()
 
+app.use((req:Request, res:Response<{message:string}>):void=>{
+  res.status(404).json({message: "No endpoint found"})
+})
+
+app.listen(PORT, ():void =>{
+  console.log("Listening on port: ", PORT)
+})
